a4835e63eb99133e88d4324db76a8d904266355d
VadimBekin <vadimbeckin@yandex.by>
2026-05-24 17:27:15 +0300

    fix6

--- hs-app/src/App/Root.tsx
+++ hs-app/src/App/Root.tsx
@@ -21,7 +21,15 @@ export default function Root() {
     return (
         <div
             style={{ backgroundImage: `url(${mainFon})`}}
-            className='fixed inset-0 w-full h-full bg-cover bg-center bg-no-repeat forward-layer'>
+            className='min-h-screen
+                        bg-cover
+                        bg-center
+                        bg-no-repeat
+                        bg-fixed
+                        max-md:bg-local
+                        w-full
+                        flex
+                        flex-col'>
 
             <nav className="relative z-50">
                 <div className={`container mx-auto px-4 py-4 flex items-center justify-between gap-8 text-white bg-gray-900/20 backdrop-blur-md border-b border-white/5 md:border-x md:border-white/5 md:rounded-b-2xl `}>
